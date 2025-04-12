2be4e730f7f57d037bfdfb4f565a6473d7fe8fd3
ArnavSharma2007 <arnav.sharma.2k7@gmail.com>
2025-04-12 15:48:16 +0530

    Refactor CodeFileOneForFocusVault.cs with new changes

--- CodeFileOneForFocusVault.cs
+++ CodeFileOneForFocusVault.cs
@@ -1,14 +1,23 @@
 using System;
 
-class ReminderApp
+class RemindeUser
 {
     static void RemindUser(string task)
     {
         Console.WriteLine($"Reminder: Time to complete '{task}'!");
     }
+}
 
-    static void Main()
-    {
-        RemindUser("Morning Exercise");
-    }
+static void TrackDeadline(string task, DateTime deadline)
+{
+    Console.WriteLine($"Task: {task}, Deadline: {deadline}");
+}
+
+static void Main()
+{
+    string task = "Morning Exercise";
+    DateTime deadline = DateTime.Now.AddHours(2);
+
+    RemindUser(task);
+    TrackDeadline(task, deadline);
 }
